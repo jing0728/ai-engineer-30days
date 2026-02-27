913155e97f002bd4b471bc5022965099d84f3d6f
jing0728 <98197500+jing0728@users.noreply.github.com>
2026-02-27 13:30:30 -0500

    module

---
+++ Python-basics/module.py
@@ -0,0 +1,11 @@
+####python模块--模块是python程序的基本组织单位。在模块中可以定义变量，函数，类，以及可以执行的代码
+import random
+####导入模块
+#import 模块名 —— import random,os -->调用方式：模块名，功能名
+#import 模块名 as 别名 —— import random as rd -->调用方式：别名，功能名
+#from 模块名 import 功能名 —— from random import randint,choice -->调用方式：功能名
+#from 模块名 import 功能名 as 别名—— from random import randint as hoice -->调用方式：别名
+#from 模块名 import * —— from random import* -->调用方式：功能名
+####自定义模块
+
+####软件包（package）
